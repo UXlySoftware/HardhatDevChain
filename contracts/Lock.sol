c41abd831599cd2562a23e7309a228c9366f7f39
TtheBC01 <tac688@alumni.stanford.edu>
2025-01-31 09:26:43 -0800

    update to contract

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -20,12 +20,14 @@ contract Lock {
         owner = payable(msg.sender);
     }
 
-    function withdraw() public {
+    function withdraw(bool useReqs) public {
         // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
         // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
 
-        require(block.timestamp >= unlockTime, "You can't withdraw yet");
-        require(msg.sender == owner, "You aren't the owner");
+        if (useReqs) {
+            require(block.timestamp >= unlockTime, "You can't withdraw yet");
+            require(msg.sender == owner, "You aren't the owner");
+        }
 
         emit Withdrawal(address(this).balance, block.timestamp);
 
