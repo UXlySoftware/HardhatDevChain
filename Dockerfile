a3a31b23a9194a121b048737b3de18eb4e2a4ce9
TtheBC01 <tac688@alumni.stanford.edu>
2025-02-10 13:02:51 -0800

    send ETH to a specific address on startup

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM node:22-slim
 
 ENV TARGET_ADDRESS='0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266'
+ENV RECEIVER_ADDRESS='0x9be99125807c97d91770e36ffd4dcb8d0c3e3049'
 
 # we need tmux for background sessions and curl for fetching avalanche tools
 RUN apt update -y \
--- ignition/modules/TestToken.ts
+++ ignition/modules/TestToken.ts
@@ -6,7 +6,10 @@ import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 const TokenModule = buildModule("TestTokenModule", (m) => {
   // Get the owner address from the environment variable
   const owner = process.env.TARGET_ADDRESS;
-  console.log("Target owner address:", owner)
+  const receiver_address = process.env.RECEIVER_ADDRESS;
+  console.log("Target owner address:", owner);
+
+  const send = m.send("SendingEth", receiver_address, 1_000_000n);
 
   const token = m.contract("TestToken", [owner]);
 
